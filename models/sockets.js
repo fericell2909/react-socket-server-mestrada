841d8a1b28a9e8f760a080b61f715ce0f6fe4084
Marco Estrada <fericell2909@gmail.com>
2022-07-27 09:37:55 -0500

    emit - bands in react - socket server

---
+++ models/sockets.js
@@ -0,0 +1,61 @@
+
+const BandList = require('./band-list');
+
+class Sockets {
+
+    constructor( io ) {
+
+        this.io = io;
+        this.bandList = new BandList();
+
+        this.socketEvents();
+    }
+
+    socketEvents() {
+        // On connection
+        this.io.on('connection', ( socket ) => {
+            console.log('Cliente conectado');
+
+            // Emitir al cliente conectado, todas las bandas actuales.
+
+            socket.emit('current-bands',this.bandList.getBands());
+
+            socket.on('votar-banda', (id) => {
+
+                this.bandList.increaseVotes(id);
+
+                this.io.emit('current-bands',this.bandList.getBands());
+
+            })
+            
+            socket.on('borrar-banda', (id) => {
+
+                this.bandList.removeBand(id);
+                this.io.emit('current-bands',this.bandList.getBands());
+            
+            });
+
+            socket.on('cambiar-nombre-banda', (band) => {
+
+                this.bandList.changeName(band.id,band.nombre);
+
+                this.io.emit('current-bands',this.bandList.getBands());
+            
+            });
+
+            socket.on('nueva-banda', ({nombre}) => {
+
+                this.bandList.addBand(nombre);
+
+                this.io.emit('current-bands',this.bandList.getBands());
+            
+            });
+            
+        });
+    }
+
+
+}
+
+
+module.exports = Sockets;
